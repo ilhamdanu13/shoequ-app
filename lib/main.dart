4e6a5c2f289d4d53d9439d4143031b3edcb04a7f
ilhamdanu <86812576+ilhamdanu13@users.noreply.github.com>
2023-03-26 06:28:28 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'theme.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+          child: Text(
+        'Halo semuanya!',
+        style: priceTextStyle.copyWith(fontSize: 30),
+      )),
+    );
+  }
+}
---
+++ lib/theme.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+Color primaryColor = Color(0xffFAA519);
+Color secondaryColor = Color(0xff003566);
+Color alerColor = Color(0xffD62828);
+Color priceColor = Color(0xff33A86FF);
+Color backgroundColor1 = Color(0xffFFFFFF);
+Color backgroundColor2 = Color(0xffE9ECEF);
+Color backgroundColor3 = Color(0xffDEE2E6);
+Color primaryTextColor = Color(0xff212529);
+Color secondaryTextColor = Color(0xff495057);
+
+TextStyle primaryTextStyle = GoogleFonts.poppins(
+  color: primaryColor,
+);
+
+TextStyle secondaryTextStyle = GoogleFonts.poppins(color: secondaryColor);
+
+TextStyle priceTextStyle = GoogleFonts.poppins(color: priceColor);
+
+FontWeight light = FontWeight.w300;
+FontWeight regular = FontWeight.w400;
+FontWeight medium = FontWeight.w500;
+FontWeight semiBold = FontWeight.w600;
+FontWeight bold = FontWeight.w700;
